32ebaa215a7dd0824b1c7eb2ed620ea5e6baefa1
Slavo <slavosarik@gmail.com>
2014-08-21 22:48:44 +0200

    parser replaced by csfd api

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,70 @@
+require "json"
+require "net/http"
+
 class Movie < ActiveRecord::Base
 
   belongs_to :channel
   has_many :movie_genres
   has_many :genres, :through => :movie_genres
 
-  def self.parse_movies
-    html_csfd = Faraday.get 'http://www.csfd.cz/film/273043-branky-body-vteriny/'
-    doc = Nokogiri::HTML(html_csfd.body)
+  def self.process_movie
+    movie = find_movie("AZ-kvíz")
+    persist_movie(movie)
 
+    movie = find_movie("El Dorado")
+    persist_movie(movie)
+
+  end
+
+  def self.find_movie(name)
+    uri = URI.parse("http://csfdapi.cz/movie?search="+URI.escape(name))
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    result = JSON.parse(http.request(request).body)
+
+    uri = URI.parse("http://csfdapi.cz/movie/"+result.first["id"].to_s)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    result = JSON.parse(http.request(request).body)
+
+    #puts result["names"]["cs"]
+    #puts result["runtime"]
+    #puts result["year"]
+    #puts result["plot"]
+    #puts result["genres"]
+    #puts result["rating"]
+    #puts result["countries"]
+
+    return result
+  end
+
+  def self.persist_movie(movie)
+
+    Movie.create!(
+        name: movie["names"]["cs"],
+        duration: movie["runtime"],
+        year: movie["year"],
+        description: movie["plot"],
+        rating: movie["rating"],
+    )
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  #BACKUP PARSOVANIE
+  def self.parse_movies(html)
+    html = Faraday.get 'http://www.csfd.cz/film/273043-branky-body-vteriny/'
+    doc = Nokogiri::HTML(html.body)
 
     doc.search('.page-content').each do |div|
 
@@ -17,7 +74,7 @@ class Movie < ActiveRecord::Base
 
       Movie.create!(
           name: div.search('h1').text.strip,
-          genre: div.search('.genre').text,
+          #genre: div.search('.genre').text,
           duration: movie_origin[2].split.first,
           year: movie_origin[1].to_i,
           description: div.search('.ct-related .content li')[0].text.strip,
@@ -26,7 +83,5 @@ class Movie < ActiveRecord::Base
       )
 
     end
-
   end
-
 end
