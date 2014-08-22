97b33bca8b6dc3095621deaf3e987de1801d9840
Slavo <slavosarik@gmail.com>
2014-08-23 00:11:08 +0200

    design fix#9

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
     #@programs = Program.joins(:movie).where(programs: {:day => Time.now.strftime("%Y-%m-%d")}).order("rating DESC").first(4)
-    @programs = Program.joins(:movie).where(programs: {:day => "22-08-2014"}).order("rating DESC").first(4)
+    @programs = Program.joins(:movie).where(programs: {:day => "2014-08-22"}).order("rating DESC").first(4)
   end
 
 end
