7f9ef18b11ec86c19a237c55e710ad7949075632
Slavo <slavosarik@gmail.com>
2014-08-22 18:32:50 +0200

    added movie list to index html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
   def index
+    @programs = Program.joins(:movie).where(programs: {:day => Time.now.strftime("%Y-%m-%d")}).order("rating DESC").first(4)
   end
 end
