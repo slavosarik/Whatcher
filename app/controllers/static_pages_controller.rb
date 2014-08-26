5274086593f8d88dafe4858d2685547b636e8cb2
slavosarik <slavosarik@gmail.com>
2014-08-26 18:18:12 +0200

    program date visible in html page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,23 +6,23 @@ class StaticPagesController < ApplicationController
     case @program_time
       when 'now'
         @programs = Program.joins(:movie).
-            where.not(movies: {:rating => nil}).where(programs: {:day => Date.today}, scheduled_time_start: (Time.now + 1.hour)..(Time.now + 3.hour)).
+            where.not(movies: {:rating => nil}).where("duration > 60"). where(day: Date.today, scheduled_time_start: (Time.now + 1.hour)..(Time.now + 3.hour)).
             order("rating DESC").first(4);
       when 'today'
         @programs = Program.joins(:movie).
-            where.not(movies: {:rating => nil}).where(programs: {:day => Date.today}).order("rating DESC").first(4);
+            where.not(movies: {:rating => nil}).where("duration > 60"). where(programs: {:day => Date.today}).order("rating DESC").first(4);
       when 'tomorow'
         @programs = Program.joins(:movie).
-            where.not(movies: {:rating => nil}).where(programs: {:day => Date.today + 1.day}).order("rating DESC").first(4);
+            where.not(movies: {:rating => nil}).where("duration > 60"). where(programs: {:day => Date.today + 1.day}).order("rating DESC").first(4);
       when 'week'
         @programs = Program.joins(:movie).
-            where.not(movies: {:rating => nil}).where(programs: {day: Date.today..(Date.today + 7.day)}).order("rating DESC").first(4);
+            where.not(movies: {:rating => nil}).where("duration > 60"). where(programs: {day: Date.today..(Date.today + 7.day)}).order("rating DESC").first(4);
       when 'archiv'
         @programs = Program.joins(:movie).
-            where.not(movies: {:rating => nil}).where(programs: {:day => Date.today}).order("rating DESC").first(4);
+            where.not(movies: {:rating => nil}).where("duration > 60"). where(programs: {:day => Date.today}).order("rating DESC").first(4);
       else
         @programs = Program.joins(:movie).
-            where.not(movies: {:rating => nil}).where(programs: {:day => Date.today}).order("rating DESC").first(4);
+            where.not(movies: {:rating => nil}).where("duration > 60"). where(programs: {:day => Date.today}).order("rating DESC").first(4);
     end
 
     #@programs = Program.joins(:movie).where.not(movies: {:rating => nil}).where(programs: {:day => @day}).order("rating DESC").first(4);
